a742c1bd1cc6c07a9f7c6c956090101fb353f397
rattanakone <rattanakone@users.noreply.github.com>
2015-10-08 16:10:28 -0700

    configured grunt build control

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function (grunt) {
   // Automatically load required grunt tasks
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin'
+    buildcontrol: 'grunt-build-control'
   });
 
   // Configurable paths
@@ -29,6 +30,21 @@ module.exports = function (grunt) {
 
     // Project settings
     config: config,
+
+     buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:rattanakone/wats2000-week1.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
