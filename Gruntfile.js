f9df770fafe2d3e340235064e406b1ba0469dc91
rattanakone <rattanakone@users.noreply.github.com>
2015-10-08 17:57:50 -0700

    added comma

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
 
   // Automatically load required grunt tasks
   require('jit-grunt')(grunt, {
-    useminPrepare: 'grunt-usemin'
+    useminPrepare: 'grunt-usemin',
     buildcontrol: 'grunt-build-control'
   });
 
